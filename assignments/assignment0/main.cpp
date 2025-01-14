5422043040744a163f6f40425435b4a659650840
Owen Stanclift <owen.stanclift@mymail.champlain.edu>
2025-01-13 20:23:13 -0500

    Finished

--- assignments/assignment0/main.cpp
+++ assignments/assignment0/main.cpp
@@ -7,9 +7,26 @@
 #include <imgui.h>
 #include <imgui_impl_glfw.h>
 #include <imgui_impl_opengl3.h>
+#include <ew/shader.h>
+#include <ew/model.h>
+#include <ew/camera.h>
+#include <ew/cameraController.h>
+#include <ew/transform.h>
+#include <ew/texture.h>
 
 void framebufferSizeCallback(GLFWwindow* window, int width, int height);
 GLFWwindow* initWindow(const char* title, int width, int height);
+ew::Camera camera;
+ew::CameraController cameraController;
+
+struct Material
+{
+	float Ka = 1.0f;
+	float Kd = 0.5f;
+	float Ks = 0.5f;
+	float Shininess = 128.0f;
+}material;
+
 void drawUI();
 
 //Global state
@@ -18,8 +35,22 @@ int screenHeight = 720;
 float prevFrameTime;
 float deltaTime;
 
+
 int main() {
 	GLFWwindow* window = initWindow("Assignment 0", screenWidth, screenHeight);
+	ew::Shader shader = ew::Shader("assets/lit.vert", "assets/lit.frag");
+	GLuint brickTexture = ew::loadTexture("assets/brick_color.jpg");
+	ew::Model monkeyModel = ew::Model("assets/suzanne.fbx");
+	ew::Transform monkeyTransform;
+	glEnable(GL_CULL_FACE);
+	glCullFace(GL_BACK);
+	glEnable(GL_DEPTH_TEST);
+	glBindTextureUnit(0, brickTexture);
+
+	camera.position = glm::vec3(0.0f, 0.0f, 5.0f);
+	camera.target = glm::vec3(0.0f, 0.0f, 0.0f);
+	camera.aspectRatio = (float)screenWidth / screenHeight;
+	camera.fov = 60.0f;
 	glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
 
 	while (!glfwWindowShouldClose(window)) {
@@ -29,9 +60,25 @@ int main() {
 		deltaTime = time - prevFrameTime;
 		prevFrameTime = time;
 
+		cameraController.move(window, &camera, deltaTime);
+
 		//RENDER
 		glClearColor(0.6f,0.8f,0.92f,1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		monkeyTransform.rotation = glm::rotate(monkeyTransform.rotation, deltaTime, glm::vec3(0.0, 1.0, 0.0));
+
+		shader.use();
+		shader.setInt("_MainTex", 0);
+		shader.setVec3("_EyePos", camera.position);
+		shader.setMat4("_Model", monkeyTransform.modelMatrix());
+		shader.setMat4("_ViewProjection", camera.projectionMatrix() * camera.viewMatrix());
+		shader.setFloat("_Material.Ka", material.Ka);
+		shader.setFloat("_Material.Kd", material.Kd);
+		shader.setFloat("_Material.Ks", material.Ks);
+		shader.setFloat("_Material.Shininess", material.Shininess);
+		monkeyModel.draw();
+
 
 		drawUI();
 
@@ -39,14 +86,29 @@ int main() {
 	}
 	printf("Shutting down...");
 }
-
+void resetCamera(ew::Camera* camera, ew::CameraController* controller)
+{
+	camera->position = glm::vec3(0, 0, 5.0f);
+	camera->target = glm::vec3(0);
+	controller->yaw = controller->pitch = 0;
+}
 void drawUI() {
 	ImGui_ImplGlfw_NewFrame();
 	ImGui_ImplOpenGL3_NewFrame();
 	ImGui::NewFrame();
 
 	ImGui::Begin("Settings");
-	ImGui::Text("Add Controls Here!");
+	if (ImGui::Button("Reset Camera"))
+	{
+		resetCamera(&camera, &cameraController);
+	}
+	if (ImGui::CollapsingHeader("Material"))
+	{
+		ImGui::SliderFloat("AmbientK", &material.Ka, 0.0f, 1.0f);
+		ImGui::SliderFloat("DiffuseK", &material.Kd, 0.0f, 1.0f);
+		ImGui::SliderFloat("SpecularK", &material.Ks, 0.0f, 1.0f);
+		ImGui::SliderFloat("Shininess", &material.Shininess, 2.0f, 1024.0f);
+	}
 	ImGui::End();
 
 	ImGui::Render();
@@ -74,6 +136,7 @@ GLFWwindow* initWindow(const char* title, int width, int height) {
 		return nullptr;
 	}
 
+
 	GLFWwindow* window = glfwCreateWindow(width, height, title, NULL, NULL);
 	if (window == NULL) {
 		printf("GLFW failed to create window");
@@ -94,4 +157,7 @@ GLFWwindow* initWindow(const char* title, int width, int height) {
 
 	return window;
 }
+
+
+
 
